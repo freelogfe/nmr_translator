988eabff2c47ee2140918b4e0bd9cd037700f385
kai <kai@MacBook-Pro.local>
2019-11-12 11:08:33 +0800

    Update charset.test.js

--- test/compile/charset.test.js
+++ test/compile/charset.test.js
@@ -21,7 +21,7 @@ describe('字符集测试', () => {
         expect(errors).not.toBeNull();
     });
 
-    test(' presentable 不能包含 ":" 字符', () => {
+    test('presentable 不能包含 ":" 字符', () => {
         const text = `
         add #b1/m1 as :p2
           do
@@ -31,13 +31,23 @@ describe('字符集测试', () => {
         expect(errors).not.toBeNull();
     });
 
-    test(' presentable 不能包含 "@" 字符', () => {
+    test('presentable 不能包含 "@" 字符', () => {
         const text = `
-        add #b1/m1 as @p2
+        add #b1/m10 as @p2
           do
           end
         `;
         const {errors} = compile(text);
         expect(errors).not.toBeNull();
+    });
+
+    test('presentable mock release 可以是纯数字', () => {
+        const text = `
+        add $1111/2222@1.1.1 as 3333
+          do
+          end
+        `;
+        const {errors} = compile(text);
+        expect(errors).toBeNull();
     });
 });
