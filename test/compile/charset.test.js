a523dcac0774af593459f67760cce64f095753a5
kai <kai@MacBook-Pro.local>
2019-11-12 11:04:15 +0800

    添加字符集测试

--- test/compile/charset.test.js
+++ test/compile/charset.test.js
@@ -1,17 +1,43 @@
-const text1 = `
-add $:u1/r1 as p1
-  do
-  end
-`;
-
-const text2 = `
-add #:b1/m1 as p2
-  do
-  end
-`;
+const {compile} = require('../../lib');
 
 describe('字符集测试', () => {
-    test('TODO:', () => {
-        expect(null).toBeNull();
+    test('发行不能包含 ":" 字符', () => {
+        const text = `
+        add $:u1/r1 as p1
+          do
+          end
+        `;
+        const {errors} = compile(text);
+        expect(errors).not.toBeNull();
+    });
+
+    test('mock 不能包含 ":" 字符', () => {
+        const text = `
+        add #:b1/m1 as p2
+          do
+          end
+        `;
+        const {errors} = compile(text);
+        expect(errors).not.toBeNull();
+    });
+
+    test(' presentable 不能包含 ":" 字符', () => {
+        const text = `
+        add #b1/m1 as :p2
+          do
+          end
+        `;
+        const {errors} = compile(text);
+        expect(errors).not.toBeNull();
+    });
+
+    test(' presentable 不能包含 "@" 字符', () => {
+        const text = `
+        add #b1/m1 as @p2
+          do
+          end
+        `;
+        const {errors} = compile(text);
+        expect(errors).not.toBeNull();
     });
 });
