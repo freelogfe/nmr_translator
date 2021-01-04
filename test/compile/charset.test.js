bfbba06fff6c19c6a60c9b20fe74d8c40fddb9c3
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2021-01-04 14:56:14 +0800

    Update charset.test.js

--- test/compile/charset.test.js
+++ test/compile/charset.test.js
@@ -91,13 +91,36 @@ describe('字符集测试', () => {
 
     describe(`所有名字都可以使用"."`, () => {
         const text = `
-        add $u.1/r.1.@1.1.1 as p1.
+        add $u.1./r.1. as p.1.
+          do
+          end
+          
+        add #b.1./o.1. as p.2.
           do
           end
         `;
 
         const {errors, rules, errorObjects} = compile(text);
-        console.log(rules, 'rules');
+        // console.log(rules, 'rules');
+        test('资源名称、对象名称和展品名称可以包含"."字符，不会报错1', () => {
+            // console.log(errors, 'EEEEEEEEEE');
+            expect(errors).toBeNull();
+        });
+
+        test('资源名称和展品名称可以包含"."字符，不会报错1', () => {
+            expect(errorObjects).toBeNull();
+        });
+    });
+
+    describe(`资源名称中可以使用“@”来添加版本`, () => {
+        const text = `
+        add $u1/r1@1.1.1 as p1
+          do
+          end
+        `;
+
+        const {errors, rules, errorObjects} = compile(text);
+        // console.log(rules, 'rules');
         test('没有操作，不会报错1', () => {
             // console.log(errors, 'EEEEEEEEEE');
             expect(errors).toBeNull();
@@ -106,5 +129,6 @@ describe('字符集测试', () => {
         test('没有操作，不会报错2', () => {
             expect(errorObjects).toBeNull();
         });
+
     });
 });
